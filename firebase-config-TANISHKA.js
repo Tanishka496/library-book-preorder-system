769f15d7187a778ae6f0f2a8ff9c924c2a65526e
Tanishka496 <tanishkakathervel496@gmail.com>
2026-01-02 19:19:40 +0530

    Main commit

---
+++ firebase-config-TANISHKA.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBYZYwrbWMezswKCKrYSdF8O0pePIb-exI",
+  authDomain: "libraty-preorder-system.firebaseapp.com",
+  projectId: "libraty-preorder-system",
+  storageBucket: "libraty-preorder-system.firebasestorage.app",
+  messagingSenderId: "694437596920",
+  appId: "1:694437596920:web:6760ffe7ecc58f8458f50d"
+};
--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyBYZYwrbWMezswKCKrYSdF8O0pePIb-exI",
-  authDomain: "libraty-preorder-system.firebaseapp.com",
-  projectId: "libraty-preorder-system",
-  storageBucket: "libraty-preorder-system.firebasestorage.app",
+  authDomain: "library-book-preorder-system.firebaseapp.com",
+  projectId: "library-book-preorder-system",
+  storageBucket: "library-book-preorder-system.appspot.com",
   messagingSenderId: "694437596920",
   appId: "1:694437596920:web:6760ffe7ecc58f8458f50d"
 };
